846561c3567fe7f9d667feb3b86b8200ec70072e
mgjeffries <gib.jeffries@gmail.com>
2022-04-28 21:01:53 -0500

    require terms agreement in order to submit email

--- home.js
+++ home.js
@@ -1,7 +1,15 @@
 document.querySelector("#submit-email").addEventListener("click", e => {
   e.preventDefault();
-  submitEmail();
+  const isTermsAgreed = document.querySelector("#Terms").checked;
+  if(isTermsAgreed){
+    submitEmail();
+  }
+  else {
+    window.alert("You must agree to the terms in order to print")
+  }
 });
+
+document.getElementById("Terms").required = true; 
 
 function submitEmail(){
   console.log("submitting email")
