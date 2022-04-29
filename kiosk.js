a2351480f671d24b787c87bce9334b7b1285e281
mgjeffries <gib.jeffries@gmail.com>
2022-04-29 13:54:48 -0500

    fix issue with image file name spaces by giving each file an id number

--- kiosk.js
+++ kiosk.js
@@ -1,5 +1,5 @@
-const KIOSK_API_KEY = "FF4D321CD33F47A7BE64ADFD8429B931";
-const KIOSK_HOST_NAME = "http://raspberrypi.local";
+const KIOSK_API_KEY = "AC2A27BA72C541EFB2E52AAE3D001AB1";
+const KIOSK_HOST_NAME = "http://raspi.local";
 const MAX_QUEUE_LENGTH = 6;
 
 async function createCard(imageAssets) {
@@ -16,7 +16,7 @@ async function createCard(imageAssets) {
       <img src="${i.imagePath}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${i.title}</h5>
-          <button type="button" class="btn btn-primary" onclick=addPrintToQueue("${i.octoFilePath}")>Print</button>
+          <button type="button" class="btn btn-primary" onclick=addPrintToQueue("${i.id}")>Print</button>
         </div>
     </div>
     `;
@@ -25,7 +25,8 @@ async function createCard(imageAssets) {
 }
 
 
-async function addPrintToQueue(filepath) {
+async function addPrintToQueue(fileID) {
+  filepath = imageAssets.find(i => i.id===fileID);
   let searchParams = new URLSearchParams(window.location.search);
   // searchParams.has('email')
   let email = searchParams.get("email");
@@ -86,56 +87,67 @@ async function isPrintQueueFull(){
 
 const imageAssets = [
   {
+    id: 1,
     imagePath: "assets/img/CaliCat.png",
     octoFilePath: "Cali Cat_55m.gcode",
     title: "Cali Cat",
   },
   {
+    id: 2,
     imagePath: "assets/img/CrystalGem.png",
     octoFilePath: "Crystal Gem_48m.gcode",
     title: "Crystal Gem",
   },
   {
+    id: 3,
     imagePath: "assets/img/20die.png",
     octoFilePath: "D20 Dice_59m.gcode",
     title: "20-sided die",
   },
   {
+    id: 4,
     imagePath: "assets/img/Fidgetgear.png",
     octoFilePath: "Fidget Gear_1h10m.gcode",
     title: "Fidget Gears",
   },
   {
+    id: 4,
     imagePath: "assets/img/FlexSnake.png",
     octoFilePath: "Flex Snake_1h12m.gcode",
     title: "Flexible Snake",
   },
   {
+    id: 5,
     imagePath: "assets/img/OwlBookmark.png",
     octoFilePath: "Owl Bookmark_1h2m.gcode",
     title: "Owl Bookmark",
   },
   {
+    id: 6,
     imagePath: "assets/img/PoopEmoji.png",
     octoFilePath: "Poop Emoji_56m.gcode",
     title: "Poopmoji",
   },
   {
+    id: 7,
     imagePath: "assets/img/Spinner.png",
     octoFilePath: "Spinner_58m.gcode",
     title: "Spinner Top",
   },
   {
+    id: 8,
     imagePath: "assets/img/XWingKit.png",
     octoFilePath: "XWing Kit_57m.gcode",
     title: "X-Wing - Star Fighter",
   },
   {
+    id: 9,
     imagePath: "assets/img/TinyBot.png",
     octoFilePath: "Tiny Bot_1h3m.gcode",
     title: "Tiny Bot Set",
   },
   {
+    id: 10,
     imagePath: "assets/img/microsaur.png",
     octoFilePath: "Dino Clip_1h0m.gcode",
     title: "Microsauraus Rex",
